129298ed585cdc780a3afaf4994cde4f040776d2
Rotciv18 <v.rotciv@hotmail.com>
2019-04-20 10:33:46 -0300

    Add files via upload

---
+++ tsp.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int calc_distance (float x1, float x2, float y1, float y2){
+    float d = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+    return round(sqrt(d));
+}
+
+void mount_matrix (int n, int M[n][n], float x[n], float y[n]){
+    int i, j;
+    int start = 0;
+    for (i = 0; i < n - 1 ; i++)
+    {
+        for (j = start; j < n ; j++){
+            if (i == j){
+                M[i][j] = M[j][i] = 0;
+                continue;
+            }
+            M[i][j] = M[j][i] = calc_distance(x[i], x[j], y[i], y[j]);
+        }
+        start++;
+    }
+    for (i = 0; i < n ; i++){
+        for (j = 0; j < n; j++){
+            if (i == j){
+                printf (" %d ", M[i][j]);
+                continue;
+            }
+            printf(" %d ", M[i][j]);
+        }
+        printf ("\n");
+    }
+}
+
+int main (){
+    FILE* file = fopen ("bayg29.txt", "r"); //abre o arquivo passado como argumento
+    if (file == NULL)
+    {
+      printf ("Falha ao tentar ler arquivo.");
+      return 1;
+    }
+    int n;
+    int i = 0;
+    const char *s[100];
+
+    while (fscanf(file, "%s", &s[i])){
+        if (!strcmp(&s[i], "DIMENSION:")){
+            fscanf(file, "%d", &n);
+            break;
+        }
+        ++i;
+    }
+
+    printf ("%s %d", &s[i], n);
+    const char *s1[2000];
+
+    while (fscanf(file, "%s", &s1[i])){
+        if (!strcmp(&s1[i], "DISPLAY_DATA_SECTION")){
+            break;
+        }
+        ++i;
+    }
+    int index[n];
+    float x[n];
+    float y[n];
+    for (i = 0; i<n ; i++){
+        fscanf(file, "%d", &index[i]);
+        //printf("\n%d", index[i]);
+        fscanf(file, "%f", &x[i]);
+        fscanf(file, "%f", &y[i]);
+    }
+    for (i = 0; i<n;i++){
+        printf ("\n%d   %f    %f", index[i], x[i], y[i]);
+    }
+
+    int M[n][n];
+    mount_matrix(n, M, x, y);
+}
